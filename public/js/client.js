c24d19a2a136f54a120179e5632a20f97c89518c
Harikrushna V. Adiecha <adiechahari@gmail.com>
2019-10-21 18:20:18 +0530

    added few features

--- public/js/client.js
+++ public/js/client.js
@@ -1,10 +1,60 @@
 const socket = io();
 
 // Connection share
-name = prompt("Name please:")
-socket.emit('register', name)
+// name = prompt("Name please:")
+// socket.emit('register', name)
 
-socket.on('list-user', name => {
-	$("#players").append($("<p>", {text: name}))
+// socket.on('list-user', name => {
+//  $("#players").append($("<p>", {text: name}))
+// })
+
+// socket.on('unlist-user', name => {
+//  $("#players p").each((a,b) => {if($(b).text() == name) $(b).remove()})
+// })
+
+
+function generateUid(n)
+{
+  let list = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let str = ''
+  while(str.length < n)
+  {
+    str += list.charAt(Math.floor(Math.random() * list.length))
+  }
+  return str;
+}
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    joined: false,
+    myname: 'Harikrushna',
+    gameId: '',
+    playerId: '',
+    showing: '',
+    perl: '',
+    players: [],
+    data: {}
+  },
+  mounted()
+  {
+    this.gameId = $("body[game]").attr('game')
+    this.playerId = generateUid(13)
+
+    // register socket listeners
+    socket.on('player-list-update', (list) => this.players = list)
+  },
+  methods: {
+    register()
+    {
+      if(this.gameId.length != 10)  return alert("Sorry, Seems like you are not on the correct page.")
+      socket.emit('register', {name: this.myname, id: this.playerId, gid: this.gameId })
+      this.joined = true
+    },
+    toggle(p)
+    {
+      this.showing = this.showing == p ? '': p
+    }
+  }
 })
-
--- socket-setup.js
+++ socket-setup.js
@@ -1,15 +1,64 @@
+const util = require('./util')
+
+var data = {}
+
+function collectPlayerList(game)
+{
+  return data[game]['players']
+}
+
+function createGame(game)
+{
+  if (!data.hasOwnProperty(game)) {
+    data[game] = {
+      players: {},
+      perls: []
+    }
+  }
+}
+
+function addPlayer(game, player)
+{
+  // Find or create Game
+  createGame(game)
+  if(!data[game]['players'].hasOwnProperty(player.id))
+  {
+    data[game]['players'][player.id] = {
+      name: player.name
+    }
+  }
+}
+
+function removePlayer(game, player)
+{
+  if (data.hasOwnProperty(game))
+  {
+    if(data[game]['players'].hasOwnProperty(player))
+    {
+      delete data[game]['players'][player]
+    }
+  }
+}
+
 module.exports = function(io)
 {
   io.on('connection', socket => {
 
     console.log('a user connected');
 
-    socket.on('register', (name) => {
+    socket.on('register', ({name, id, gid}) => {
       console.log(name + " has been registerd")
-      io.emit('list-user', name)
+      socket.puid = id
+      socket.gid = gid
+      addPlayer(gid, {id, name});
+      plist = collectPlayerList(gid)
+      io.emit('player-list-update', plist)
     })
 
-    socket.on('disconnect', () => console.log('user disconnected'));
+    socket.on('disconnect', () => {
+      removePlayer(socket.gid, socket.puid)
+      if (socket.puid != null) io.emit('user-disconnected', socket.puid)
+    });
 
   });
 }
